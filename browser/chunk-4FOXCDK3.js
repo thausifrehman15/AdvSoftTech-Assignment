8dfca13809283e3f685e3812544c023f109d245d
thausifrehman15 <41261168+thausifrehman15@users.noreply.github.com>
2025-05-25 16:26:04 +0000

    Deploying to gh-pages from @ thausifrehman15/AdvSoftTech-Assignment@b29dd546b125e4b04387326f60692bdaa0977a33 🚀

---
+++ browser/chunk-4FOXCDK3.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var o=[{path:"",loadComponent:()=>import("./chunk-FFMTA6TN.js").then(t=>t.WidgetsComponent),data:{title:"Widgets"}}];export{o as routes};
--- browser/chunk-PBGU2RMX.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var o=[{path:"",loadComponent:()=>import("./chunk-XUES7ECI.js").then(t=>t.WidgetsComponent),data:{title:"Widgets"}}];export{o as routes};
