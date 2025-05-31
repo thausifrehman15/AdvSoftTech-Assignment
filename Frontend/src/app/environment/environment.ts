d76483c4aed1cb903e9f4fa9fbf08c1b3018c5a8
thausifrehman15 <thausifrehman.m@gmail.com>
2025-05-31 17:14:19 +0200

    Fix: Update API URL in development environment configuration

--- Frontend/src/app/environment/environment.ts
+++ Frontend/src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://127.0.0.1:5000'
+    apiUrl: 'https://advsofttech-assignment-2.onrender.com'
 };
 
 export const environmentProd = {
